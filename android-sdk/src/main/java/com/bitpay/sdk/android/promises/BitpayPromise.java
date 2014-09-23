martindale/bitpay-android-sdk

BitPay Android SDK goes live

android-sdk/src/main/java/com/bitpay/androidsdk/android/promises/BitpayPromise.java
@@ -1,0 +1,10 @@
+package com.bitpay.androidsdk.android.promises;
+
+import com.bitpay.androidsdk.android.interfaces.PromiseCallback;
+
+/**
+ * Created by eordano on 9/11/14.
+ */
+public abstract class BitpayPromise<Promised> {
+    public abstract void then(PromiseCallback<Promised> callback);
+}

android-sdk/src/main/java/com/bitpay/androidsdk/controller/KeyUtils.java
@@ -1,0 +1,110 @@
+package com.bitpay.androidsdk.controller;
+
+import com.google.bitcoin.core.Base58;
+import com.google.bitcoin.core.ECKey;
+import com.google.bitcoin.core.ECKey.ECDSASignature;
+import com.google.bitcoin.core.Sha256Hash;
+import com.google.bitcoin.core.Utils;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+public class KeyUtils {
+	
+	final private static char[] hexArray = "0123456789abcdef".toCharArray();
+	
+	public KeyUtils() {}
+
+    public static ECKey createEcKey()
+    {
+        //Default constructor uses SecureRandom numbers.
+        return new ECKey();
+    }
+
+    public static ECKey loadEcKey(String string) throws IOException
+    {
+        ECKey key = ECKey.fromASN1(hexToBytes(string));
+        return key;
+    }
+
+    public static ECKey loadFromHexaEncodedPrivateKey(String key) {
+        return new ECKey(new BigInteger(key, 16));
+    }
+    public static String exportPrivateKeyToHexa(ECKey key) {
+        return bytesToHex(key.getPrivKeyBytes());
+    }
+
+    public static String exportEcKey(ECKey ecKey) throws IOException {
+        return bytesToHex(ecKey.toASN1());
+    }
+    
+    public static String deriveSIN(ECKey ecKey) throws IllegalArgumentException
+    {
+        // Get sha256 hash and then the RIPEMD-160 hash of the public key (this call gets the result in one step).
+        byte[] pubKeyHash = ecKey.getPubKeyHash(); 
+
+        // Convert binary pubKeyHash, SINtype and version to Hex
+        String version = "0F";
+        String SINtype = "02";
+        String pubKeyHashHex = bytesToHex(pubKeyHash);
+
+        // Concatenate all three elements
+        String preSIN = version + SINtype + pubKeyHashHex;
+
+        // Convert the hex string back to binary and double sha256 hash it leaving in binary both times
+        byte[] preSINbyte = hexToBytes(preSIN);
+        byte[] hash2Bytes = Utils.doubleDigest(preSINbyte);
+
+        // Convert back to hex and take first four bytes
+        String hashString = bytesToHex(hash2Bytes);
+        String first4Bytes = hashString.substring(0, 8);
+
+        // Append first four bytes to fully appended SIN string
+        String unencoded = preSIN + first4Bytes;
+        byte[] unencodedBytes = new BigInteger(unencoded, 16).toByteArray();
+        String encoded = Base58.encode(unencodedBytes);
+
+        return encoded;
+    }
+    	
+	public static String sign(ECKey key, String input) {
+		byte[] data = input.getBytes();
+        Sha256Hash hash = Sha256Hash.create(data);
+        ECDSASignature sig = key.sign(hash, null);
+        byte[] bytes = sig.encodeToDER();
+        return bytesToHex(bytes);
+	}
+	
+    private static int getHexVal(char hex)
+    {
+        int val = (int)hex;
+        return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
+    }
+
+    public static byte[] hexToBytes(String hex) throws IllegalArgumentException
+    {
+    	char[] hexArray = hex.toCharArray();
+    	
+        if (hex.length() % 2 == 1)
+        {
+            throw new IllegalArgumentException("Error: The binary key cannot have an odd number of digits");
+        }
+        byte[] arr = new byte[hex.length() >> 1];
+
+        for (int i = 0; i < hex.length() >> 1; ++i)
+        {
+            arr[i] = (byte)((getHexVal(hexArray[i << 1]) << 4) + (getHexVal(hexArray[(i << 1) + 1])));
+        }
+        return arr;
+    }
+
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
+	}
+}

android-sdk/src/main/java/com/bitpay/androidsdk/model/InvoicePaymentUrls.java
@@ -1,0 +1,57 @@
+package com.bitpay.androidsdk.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Describes BIPs
+ */
+public class InvoicePaymentUrls {
+
+	private String _BIP21 = "";
+	private String _BIP72 = "";
+	private String _BIP72b = "";
+	private String _BIP73 = "";
+	
+    public InvoicePaymentUrls() {}
+
+    @JsonIgnore
+	public String getBIP21() {
+		return _BIP21;
+	}
+        
+    @JsonProperty("BIP21")
+	public void setBIP21(String _BIP21) {
+		this._BIP21 = _BIP21;
+	}
+
+    @JsonIgnore
+	public String getBIP72() {
+		return _BIP72;
+	}
+        
+    @JsonProperty("BIP72")
+	public void setBIP72(String _BIP72) {
+		this._BIP72 = _BIP72;
+	}
+
+    @JsonIgnore
+	public String getBIP72b() {
+		return _BIP72b;
+	}
+        
+    @JsonProperty("BIP72b")
+	public void setBIP72b(String _BIP72b) {
+		this._BIP72b = _BIP72b;
+	}
+
+    @JsonIgnore
+	public String getBIP73() {
+		return _BIP73;
+	}
+        
+    @JsonProperty("BIP73")
+	public void setBIP73(String _BIP73) {
+		this._BIP73 = _BIP73;
+	}
+}

android-sdk/src/main/java/com/bitpay/androidsdk/model/InvoiceTransaction.java
@@ -1,0 +1,75 @@
+package com.bitpay.androidsdk.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Stores information about when (in which transaction in the blockchain was an invoice paid,
+ * partially or totally.
+ */
+public class InvoiceTransaction {
+
+	private String _txid;
+	private String _type;
+	private double _amount;
+    private Integer _confirmations;
+    private String _time;
+    private String _receivedTime;
+	
+    public InvoiceTransaction() {}
+    
+    @JsonIgnore
+	public String getTxid() {
+		return _txid;
+	}
+    
+    @JsonProperty("txid")
+	public void setTxid(String _txid) {
+		this._txid = _txid;
+	}
+
+    @JsonIgnore
+	public String getType() {
+		return _type;
+	}
+    
+    @JsonProperty("type")
+	public void setType(String _type) {
+		this._type = _type;
+	}
+
+    @JsonIgnore
+	public double getAmount() {
+		return _amount;
+	}
+    
+    @JsonProperty("amount")
+	public void setAmount(double _amount) {
+		this._amount = _amount;
+	}
+
+    @JsonIgnore
+    public Integer getConfirmations() {
+        return _confirmations;
+    }
+    @JsonProperty("confirmations")
+    public void setConfirmations(Integer confirmations) {
+        this._confirmations = confirmations;
+    }
+    @JsonIgnore
+    public String getTime() {
+        return _time;
+    }
+    @JsonProperty("time")
+    public void setTime(String time) {
+        this._time = time;
+    }
+    @JsonIgnore
+    public String getReceivedTime() {
+        return _receivedTime;
+    }
+    @JsonProperty("receivedTime")
+    public void setReceivedTime(String receivedTime) {
+        this._receivedTime = receivedTime;
+    }
+}

android-sdk/src/main/java/com/bitpay/androidsdk/model/Rate.java
@@ -1,0 +1,51 @@
+package com.bitpay.androidsdk.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Stores the exchange rate for a given currency.
+ */
+public class Rate {
+	
+	private String _name;
+	private String _code;
+	private double _value;
+	
+    public Rate() {}
+    
+    @JsonIgnore
+	public String getName() {
+		return _name;
+	}
+    
+    @JsonProperty("name")
+	public void setName(String _name) {
+		this._name = _name;
+	}
+
+    @JsonIgnore
+	public String getCode() {
+		return _code;
+	}
+    
+    @JsonProperty("code")
+	public void setCode(String _code) {
+		this._code = _code;
+	}
+
+    @JsonIgnore
+	public double getValue() {
+		return _value;
+	}
+    
+    @JsonProperty("rate")
+	public void setValue(double _value) {
+		this._value = _value;
+	}
+
+    @Override
+    public String toString() {
+        return _name;
+    }
+}

bitpay/android-sdk

android-sdk/src/main/java/com/bitpay/sdk/android/promises/BitpayPromise.java
@@ -1,0 +1,10 @@
+package com.bitpay.sdk.android.promises;
+
+import com.bitpay.sdk.android.interfaces.PromiseCallback;
+
+/**
+ * Created by eordano on 9/11/14.
+ */
+public abstract class BitpayPromise<Promised> {
+    public abstract void then(PromiseCallback<Promised> callback);
+}

android-sdk/src/main/java/com/bitpay/sdk/controller/KeyUtils.java
@@ -1,0 +1,110 @@
+package com.bitpay.sdk.controller;
+
+import com.google.bitcoin.core.Base58;
+import com.google.bitcoin.core.ECKey;
+import com.google.bitcoin.core.ECKey.ECDSASignature;
+import com.google.bitcoin.core.Sha256Hash;
+import com.google.bitcoin.core.Utils;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+public class KeyUtils {
+	
+	final private static char[] hexArray = "0123456789abcdef".toCharArray();
+	
+	public KeyUtils() {}
+
+    public static ECKey createEcKey()
+    {
+        //Default constructor uses SecureRandom numbers.
+        return new ECKey();
+    }
+
+    public static ECKey loadEcKey(String string) throws IOException
+    {
+        ECKey key = ECKey.fromASN1(hexToBytes(string));
+        return key;
+    }
+
+    public static ECKey loadFromHexaEncodedPrivateKey(String key) {
+        return new ECKey(new BigInteger(key, 16));
+    }
+    public static String exportPrivateKeyToHexa(ECKey key) {
+        return bytesToHex(key.getPrivKeyBytes());
+    }
+
+    public static String exportEcKey(ECKey ecKey) throws IOException {
+        return bytesToHex(ecKey.toASN1());
+    }
+    
+    public static String deriveSIN(ECKey ecKey) throws IllegalArgumentException
+    {
+        // Get sha256 hash and then the RIPEMD-160 hash of the public key (this call gets the result in one step).
+        byte[] pubKeyHash = ecKey.getPubKeyHash(); 
+
+        // Convert binary pubKeyHash, SINtype and version to Hex
+        String version = "0F";
+        String SINtype = "02";
+        String pubKeyHashHex = bytesToHex(pubKeyHash);
+
+        // Concatenate all three elements
+        String preSIN = version + SINtype + pubKeyHashHex;
+
+        // Convert the hex string back to binary and double sha256 hash it leaving in binary both times
+        byte[] preSINbyte = hexToBytes(preSIN);
+        byte[] hash2Bytes = Utils.doubleDigest(preSINbyte);
+
+        // Convert back to hex and take first four bytes
+        String hashString = bytesToHex(hash2Bytes);
+        String first4Bytes = hashString.substring(0, 8);
+
+        // Append first four bytes to fully appended SIN string
+        String unencoded = preSIN + first4Bytes;
+        byte[] unencodedBytes = new BigInteger(unencoded, 16).toByteArray();
+        String encoded = Base58.encode(unencodedBytes);
+
+        return encoded;
+    }
+    	
+	public static String sign(ECKey key, String input) {
+		byte[] data = input.getBytes();
+        Sha256Hash hash = Sha256Hash.create(data);
+        ECDSASignature sig = key.sign(hash, null);
+        byte[] bytes = sig.encodeToDER();
+        return bytesToHex(bytes);
+	}
+	
+    private static int getHexVal(char hex)
+    {
+        int val = (int)hex;
+        return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
+    }
+
+    public static byte[] hexToBytes(String hex) throws IllegalArgumentException
+    {
+    	char[] hexArray = hex.toCharArray();
+    	
+        if (hex.length() % 2 == 1)
+        {
+            throw new IllegalArgumentException("Error: The binary key cannot have an odd number of digits");
+        }
+        byte[] arr = new byte[hex.length() >> 1];
+
+        for (int i = 0; i < hex.length() >> 1; ++i)
+        {
+            arr[i] = (byte)((getHexVal(hexArray[i << 1]) << 4) + (getHexVal(hexArray[(i << 1) + 1])));
+        }
+        return arr;
+    }
+
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
+	}
+}

android-sdk/src/main/java/com/bitpay/sdk/model/InvoicePaymentUrls.java
@@ -1,0 +1,57 @@
+package com.bitpay.sdk.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Describes BIPs
+ */
+public class InvoicePaymentUrls {
+
+	private String _BIP21 = "";
+	private String _BIP72 = "";
+	private String _BIP72b = "";
+	private String _BIP73 = "";
+	
+    public InvoicePaymentUrls() {}
+
+    @JsonIgnore
+	public String getBIP21() {
+		return _BIP21;
+	}
+        
+    @JsonProperty("BIP21")
+	public void setBIP21(String _BIP21) {
+		this._BIP21 = _BIP21;
+	}
+
+    @JsonIgnore
+	public String getBIP72() {
+		return _BIP72;
+	}
+        
+    @JsonProperty("BIP72")
+	public void setBIP72(String _BIP72) {
+		this._BIP72 = _BIP72;
+	}
+
+    @JsonIgnore
+	public String getBIP72b() {
+		return _BIP72b;
+	}
+        
+    @JsonProperty("BIP72b")
+	public void setBIP72b(String _BIP72b) {
+		this._BIP72b = _BIP72b;
+	}
+
+    @JsonIgnore
+	public String getBIP73() {
+		return _BIP73;
+	}
+        
+    @JsonProperty("BIP73")
+	public void setBIP73(String _BIP73) {
+		this._BIP73 = _BIP73;
+	}
+}

android-sdk/src/main/java/com/bitpay/sdk/model/InvoiceTransaction.java
@@ -1,0 +1,75 @@
+package com.bitpay.sdk.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Stores information about when (in which transaction in the blockchain was an invoice paid,
+ * partially or totally.
+ */
+public class InvoiceTransaction {
+
+	private String _txid;
+	private String _type;
+	private double _amount;
+    private Integer _confirmations;
+    private String _time;
+    private String _receivedTime;
+	
+    public InvoiceTransaction() {}
+    
+    @JsonIgnore
+	public String getTxid() {
+		return _txid;
+	}
+    
+    @JsonProperty("txid")
+	public void setTxid(String _txid) {
+		this._txid = _txid;
+	}
+
+    @JsonIgnore
+	public String getType() {
+		return _type;
+	}
+    
+    @JsonProperty("type")
+	public void setType(String _type) {
+		this._type = _type;
+	}
+
+    @JsonIgnore
+	public double getAmount() {
+		return _amount;
+	}
+    
+    @JsonProperty("amount")
+	public void setAmount(double _amount) {
+		this._amount = _amount;
+	}
+
+    @JsonIgnore
+    public Integer getConfirmations() {
+        return _confirmations;
+    }
+    @JsonProperty("confirmations")
+    public void setConfirmations(Integer confirmations) {
+        this._confirmations = confirmations;
+    }
+    @JsonIgnore
+    public String getTime() {
+        return _time;
+    }
+    @JsonProperty("time")
+    public void setTime(String time) {
+        this._time = time;
+    }
+    @JsonIgnore
+    public String getReceivedTime() {
+        return _receivedTime;
+    }
+    @JsonProperty("receivedTime")
+    public void setReceivedTime(String receivedTime) {
+        this._receivedTime = receivedTime;
+    }
+}

android-sdk/src/main/java/com/bitpay/sdk/model/Rate.java
@@ -1,0 +1,51 @@
+package com.bitpay.sdk.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Stores the exchange rate for a given currency.
+ */
+public class Rate {
+	
+	private String _name;
+	private String _code;
+	private double _value;
+	
+    public Rate() {}
+    
+    @JsonIgnore
+	public String getName() {
+		return _name;
+	}
+    
+    @JsonProperty("name")
+	public void setName(String _name) {
+		this._name = _name;
+	}
+
+    @JsonIgnore
+	public String getCode() {
+		return _code;
+	}
+    
+    @JsonProperty("code")
+	public void setCode(String _code) {
+		this._code = _code;
+	}
+
+    @JsonIgnore
+	public double getValue() {
+		return _value;
+	}
+    
+    @JsonProperty("rate")
+	public void setValue(double _value) {
+		this._value = _value;
+	}
+
+    @Override
+    public String toString() {
+        return _name;
+    }
+}